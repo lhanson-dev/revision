b1ddb6310b71b703fd56e907e419635c318cd519
lhanson-dev <lhanson@gmail.com>
2026-08-23 08:56:35 +0100

    Classify Ask REV CTA as semantic runtime layer

--- src/main.tsx
+++ src/main.tsx
@@ -31,8 +31,8 @@ import './app/interface-subjects-course.css'
 import './app/interface-learn-practice.css'
 import './app/interface-exam-experience.css'
 import './app/interface-admin.css'
-import './app/interface-theme-integrity.css'
 import './app/ask-rev-cta.css'
+import './app/interface-theme-integrity.css'
 
 const root = document.getElementById('root')
 
