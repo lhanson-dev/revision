c5f165dbd1f9b1d5daf0719ff14d03c6b3230dd0
lhanson-dev <lhanson@gmail.com>
2026-08-25 23:21:32 +0100

    Satisfy strict unused-variable lint rule

--- src/content-factory/assessment-and-marking.test.ts
+++ src/content-factory/assessment-and-marking.test.ts
@@ -449,7 +449,7 @@ describe('Content Factory assessment and Marking Pack factory', () => {
         itemCalls += 1
         throw new Error('assessment item should be reused')
       },
-      generateMarkingPack: async (_input) => {
+      generateMarkingPack: async () => {
         markingCalls += 1
         return success(`marking-pack-resume-${markingCalls}`, markingOutput)
       },
