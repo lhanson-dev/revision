73b02ce2dfea60b68f9fa9429d2cd5f4c5ca2344
lhanson-dev <lhanson@gmail.com>
2026-09-26 12:43:17 +0100

    Load shared learner content canvas layer

--- src/main.tsx
+++ src/main.tsx
@@ -41,6 +41,7 @@ import './app/subject-accents.css'
 import './app/returning-home.css'
 import './app/returning-home-fidelity.css'
 import './app/course-overview-rev-feature.css'
+import './app/interface-content-canvas.css'
 
 const root = document.getElementById('root')
 
